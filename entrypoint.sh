06b077e321ade2cad97b1b23ae19abeb1a5452a0
Kirill Sumorokov <sumorokov.k@gmail.com>
2020-10-23 15:29:21 +0500

    Omit curl output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-export VAULT_TOKEN="$(curl --request POST --data '{"role_id": "'$VAULT_ROLE_ID'", "secret_id": "'$VAULT_SECRET_ID'"}' $VAULT_ADDR/v1/auth/approle/login | jq .auth.client_token | sed -e 's/^"//' -e 's/"$//')"
+export VAULT_TOKEN="$(curl -q --request POST --data '{"role_id": "'$VAULT_ROLE_ID'", "secret_id": "'$VAULT_SECRET_ID'"}' $VAULT_ADDR/v1/auth/approle/login | jq .auth.client_token | sed -e 's/^"//' -e 's/"$//')"
 
 ansible-playbook $@
 
